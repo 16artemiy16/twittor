fd2e50e50fec55b371cb12cfa40a50e4365537a1
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-12-10 15:31:25 +0300

    Implement Trends section (Explore page)

---
+++ components/ContentCard.vue
@@ -0,0 +1,111 @@
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+interface ActionI {
+  icon: string;
+  text: string;
+  onClick?: () => void;
+}
+
+@Component
+export default class ContentCard extends Vue {
+  @Prop(String)
+  readonly topText: string | undefined;
+
+  @Prop({ type: String, required: true })
+  readonly mainText: string | undefined;
+
+  @Prop(String)
+  readonly bottomText: string | undefined;
+
+  @Prop({ type: Array, default: () => [] })
+  readonly actions!: ActionI[];
+
+  get hasActions(): boolean {
+    return !!this.actions.length;
+  }
+
+  onActionClick(action: ActionI) {
+    if (action.onClick) {
+      action.onClick();
+    }
+  }
+}
+</script>
+
+
+<template>
+  <article class="card" data-app>
+    <div v-if="topText" class="card__top-text">{{ topText }}</div>
+    <div v-if="mainText" class="card__main-text">{{ mainText }}</div>
+    <div v-if="bottomText" class="card__bottom-text">{{ bottomText }}</div>
+    <v-menu v-if="hasActions" bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="actions-menu"
+          text
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-dots-horizontal</v-icon>
+        </v-btn>
+      </template>
+      <v-list light>
+        <v-list-item
+          v-for="(action, index) in actions"
+          :key="index"
+          @click="onActionClick(action)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ action.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ action.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  padding: 1rem;
+
+  &:hover {
+    background: $background-hover-pale;
+  }
+
+  &__main-text {
+    font-weight: bold;
+    margin: 0.25em 0;
+  }
+
+  &__top-text, &__bottom-text {
+    font-size: 0.85em;
+  }
+}
+
+.actions-menu {
+  position: absolute;
+  right: .5rem;
+  top: .5rem;
+}
+
+.v-list-item {
+  &:hover {
+    background: $background-hover-pale;
+  }
+}
+
+.v-list-item__icon {
+  padding-right: 1rem;
+}
+</style>
--- pages/Authed/Explore.vue
+++ pages/Authed/Explore.vue
@@ -1,11 +1,83 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import ContentCard from '~/components/ContentCard.vue';
 
-@Component
-export default class Explore extends Vue {}
+@Component({
+  components: {
+    ContentCard
+  }
+})
+export default class Explore extends Vue {
+  trends = [
+    { id: '1', title: 'First trend' },
+    { id: '2', title: 'Second trend' },
+    { id: '3', title: 'Third trend' },
+  ];
+
+  reportNotInteresting(id: string) {
+  }
+
+  reportSpam(id: string) {
+  }
+}
 </script>
 
 <template>
-  <div>Explore</div>
+  <div class="explore">
+    <header class="explore__section explore__header">
+      <v-text-field
+        hide-details
+        outlined
+        clearable
+        rounded
+        prepend-inner-icon="mdi-magnify"
+        placeholder="Search Twittor"
+      ></v-text-field>
+    </header>
+    <section class="explore__section explore__trends">
+      <h1>Trends for you</h1>
+      <ContentCard
+        v-for="trend in trends"
+        :key="trend.id"
+        :main-text="trend.title"
+        :actions="[
+          { icon: 'mdi-emoticon-angry-outline', text: 'It \'s not interesting', onClick: () => reportNotInteresting(trend.id) },
+          { icon: 'mdi-alert-circle-outline', text: 'Report Spam', onClick: () => reportSpam(trend.id) },
+        ]"
+        top-text="Trending"
+        bottom-text="3,504 Twits"
+      />
+      <ContentCard
+        class="link"
+        main-text="Show more"
+      />
+    </section>
+    <section class="explore__section explore__posts"></section>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.link {
+  color: $link;
+}
+.explore {
+  &__header, &__trends {
+    border-bottom: 1px $grey solid;
+  }
+
+  &__section {
+    padding: .5rem 0;
+  }
+
+  &__header {
+    padding: .5rem 1rem;
+  }
+
+  &__trends {
+    h1, div {
+      padding: .5em 1rem;
+    }
+  }
+}
+</style>
