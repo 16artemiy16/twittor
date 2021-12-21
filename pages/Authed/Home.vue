dcee08a8ff168a4626d3a018ccbb3b7934487c61
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-12-21 15:53:01 +0300

    Add tweet form to Home page

--- pages/Authed/Home.vue
+++ pages/Authed/Home.vue
@@ -1,11 +1,35 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import TweetForm from '~/components/TweetForm.vue';
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: {
+    TweetForm: () => import('~/components/TweetForm.vue'),
+  },
+})
+export default class Home extends Vue {
+  tweetModel: { text: string } = { text: '' };
+}
 </script>
 
 <template>
-  <div>Home</div>
+  <div class="home">
+    <header class="header">
+      <h2>Home</h2>
+    </header>
+    <div class="new-tweet">
+      <TweetForm v-model="tweetModel" />
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.header {
+  padding: 1rem;
+}
+.new-tweet {
+  padding: 1rem;
+  border-bottom: 1px $grey solid;
+}
+</style>
