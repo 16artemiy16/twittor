38cd071fa0bc66d6254a747a1483b03813cb7589
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-12-24 16:49:48 +0300

    Fix jest

--- components/tests/TweetItem.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('TweetItem', () => {
-  it('test', () => {
-    expect(true).toBe(true);
-  });
-});
---
+++ test/TweetItem.spec.ts
@@ -0,0 +1,38 @@
+import { shallowMount, Wrapper } from '@vue/test-utils';
+import { TweetI } from '~/interfaces/tweet.interface';
+import TweetItem from '~/components/TweetItem/TweetItem.vue';
+
+const TWEET: TweetI = {
+  id: 'a',
+  body: 'Hello! Welcome here!',
+  user: {
+    name: 'John',
+    login: 'john',
+    img: 'http://john.com/my-img.png',
+  },
+  likes: {
+    total: 15,
+    isLikedByMe: false,
+  },
+}
+
+describe('TweetItem', () => {
+  let wrapper!: Wrapper<TweetItem>;
+
+  beforeEach(() => {
+    wrapper = shallowMount(TweetItem, {
+      propsData: {
+        tweet: TWEET,
+      },
+    });
+  });
+
+  describe('should render text data correctly', () => {
+    it('User name', () => {
+      expect(wrapper.get('.user-name').text()).toBe(TWEET.user.name);
+    })
+  })
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
