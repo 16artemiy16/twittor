f5f1dbcdb44a3a243ebfdff6a53dda14402aae0b
Artem Polikarpov <artemiypolikarpov@gmail.com>
2022-01-05 15:44:04 +0300

    Enable CORS and bungle errors

--- twittor-back/app.py
+++ twittor-back/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+CORS(app)
 
 api = Api(app)
 jwt = JWTManager(app)
--- twittor-back/config.py
+++ twittor-back/config.py
@@ -1,6 +1,7 @@
 class DevConfig:
     TESTING = True
     DEBUG = True
+    BUNDLE_ERRORS = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'secret'
