dbe910eb6f24953742702456d32037db06d24b45
Artem Polikarpov <artemiypolikarpov@gmail.com>
2022-04-29 13:17:07 +0300

    Add auto-redirect when 404 route

--- twittor-front-2/src/router/index.ts
+++ twittor-front-2/src/router/index.ts
@@ -5,6 +5,10 @@ const routes: Array<RouteRecordRaw> = [
     path: '/sign-in',
     name: 'login',
     component: () => import('@/views/SignInPage.vue' /* webpackChunkName: "SignInPage" */),
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/sign-in',
   }
 ]
 
